7ebbe8b8202ed78cb2d5739af0a390c376eb9c8c
Rafael <rafaelgatti@claretiano.edu.br>
2021-03-23 11:04:43 -0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models: {
+    'transaction': Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Escola JR',
+          type: 'withdraw',
+          amount: 400,
+          category: 'EDUCACAO',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Proventos',
+          type: 'deposit',
+          amount: 5000,
+          category: 'SALARIO',
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+
+  routes () {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +52,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
